863faa79530ad5df414ed2659cde3168f896bc8a
Lucas <lucas.mercier@viacesi.fr>
2021-03-25 16:19:38 +0100

    create kernel config to stock all routes collections

---
+++ app/Config/KernelConfig.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Config;
+
+class KernelConfig
+{
+
+    /**
+     * @var string[]
+     */
+    private array $routes = [
+        ApiRouteList::class
+    ];
+
+    /**
+     * @return string[]
+     */
+    public function getRoutes(): array
+    {
+        return $this->routes;
+    }
+}
