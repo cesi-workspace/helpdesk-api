853c38985e7c086e15d762c5e1f057a3089571da
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-05-18 11:30:54 +0200

    make basic action

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -2,7 +2,18 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
 class HomeController
 {
 
+    /**
+     * @Route("/blog", name="blog_list", methods={"GET"})
+     */
+    public function index(Request $request)
+    {
+        return new JsonResponse(['body' => ['test' => true]], 400);
+    }
 }
