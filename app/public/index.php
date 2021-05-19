32ecc94cda31d467aa43f03702dc942370af514a
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-05-19 10:22:02 +0200

    Use import statement for DI namespace

--- app/public/index.php
+++ app/public/index.php
@@ -1,12 +1,13 @@
 <?php
 
+use DI\ContainerBuilder;
 use Kernel\App;
 use Symfony\Component\HttpFoundation\Request;
 
 require '../../vendor/autoload.php';
 
-$containerBuilder = new \DI\ContainerBuilder();
-$containerBuilder->addDefinitions(dirname(__DIR__) . "/../config/container.php");
+$containerBuilder = new ContainerBuilder();
+$containerBuilder->addDefinitions(dirname(__DIR__) . "/../config/config.php");
 
 $app = new App($containerBuilder->build());
 $request = Request::createFromGlobals();
