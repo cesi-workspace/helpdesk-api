41be726f5502219c7beabfca07d95c9c50508aa2
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-05-21 14:37:20 +0200

    implement doctrine orm

--- config/config.php
+++ config/config.php
@@ -1,4 +1,26 @@
 <?php
 
+use Doctrine\ORM\Configuration;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Setup;
+
+use function DI\env;
+use function DI\factory;
+use function DI\get;
+
 return [
+    "env" => env("env", "development"),
+    "path.entities" => [ dirname(__DIR__) . "/app/Entity" ],
+    "db" => [
+        "driver" => env("db.driver", "pdo_mysql"),
+        "user" => env("db.username", "root"),
+        "password" => env("db.password", ""),
+        "dbname" => env("db.name", "cesi")
+    ],
+    Configuration::class => factory([Setup::class, 'createAnnotationMetadataConfiguration'])
+        ->parameter("paths", get("path.entities"))
+        ->parameter("isDevMode", get("env") == "development"),
+    EntityManager::class => factory([EntityManager::class, 'create'])
+        ->parameter('connection', get('db'))
+        ->parameter('config', get(Configuration::class)),
 ];
