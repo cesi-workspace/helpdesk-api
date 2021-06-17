db8dec2b43b20005a2da56077b9c3e3a623285d7
Lucas Mercier <lucas.mercier@viacesi.fr>
2021-06-17 08:06:28 +0200

    fix: remove request from constructor so that we'll use DI

--- src/Kernel/AbstractController.php
+++ src/Kernel/AbstractController.php
@@ -2,19 +2,7 @@
 
 namespace Kernel;
 
-use Symfony\Component\HttpFoundation\Request;
-
 abstract class AbstractController
 {
-    private Request $request;
 
-    public function __construct(Request $request)
-    {
-        $this->request = $request;
-    }
-
-    protected function getRequest(): Request
-    {
-        return $this->request;
-    }
 }
