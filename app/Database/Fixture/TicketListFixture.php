1d92272735f89df16cf49099f339be7e3243704e
Lucas Mercier <lucas.mercier@viacesi.fr>
2021-06-17 15:38:26 +0200

    refactror: add user implementation

--- app/Database/Fixture/TicketListFixture.php
+++ app/Database/Fixture/TicketListFixture.php
@@ -2,7 +2,9 @@
 
 namespace App\Database\Fixture;
 
-use App\Entity\Ticket;
+use App\Domain\Entity\Ticket;
+use App\Domain\Entity\User;
+use App\Domain\Repository\IUserRepository;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Lorem;
@@ -11,11 +13,16 @@ class TicketListFixture implements FixtureInterface
 {
     public function load(ObjectManager $manager)
     {
+        /** @var IUserRepository $userRepository */
+        $userRepository = $manager->getRepository(User::class);
+        $user = $userRepository->findByName("Lucas");
+
         for ($i = 0; $i < 10; $i++) {
             $ticket = new Ticket();
             $ticket->setDescription(join("\n\n", Lorem::paragraphs()));
             $ticket->setStatus("open");
             $ticket->setTitle(Lorem::sentence(5));
+            $ticket->setUser($user);
 
             $manager->persist($ticket);
         }
